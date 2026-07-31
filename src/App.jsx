4caa06d5ebb64249467ac5f537d9a0a6822bb701
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-07-31 19:36:52 +0300

    Merge pull request #1: Replace manual data, loading and error handling in useEffect with tan…

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,51 @@
 import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
+    Navigate,
+    RouterProvider,
+    createBrowserRouter,
 } from 'react-router-dom';
 
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Navigate to="/events" />,
-  },
-  {
-    path: '/events',
-    element: <Events />,
+    {
+        path: '/',
+        element: <Navigate to="/events"/>,
+    },
+    {
+        path: '/events',
+        element: <Events/>,
 
-    children: [
-      {
-        path: '/events/new',
-        element: <NewEvent />,
-      },
-    ],
-  },
-  {
-    path: '/events/:id',
-    element: <EventDetails />,
-    children: [
-      {
-        path: '/events/:id/edit',
-        element: <EditEvent />,
-      },
-    ],
-  },
+        children: [
+            {
+                path: '/events/new',
+                element: <NewEvent/>,
+            },
+        ],
+    },
+    {
+        path: '/events/:id',
+        element: <EventDetails/>,
+        children: [
+            {
+                path: '/events/:id/edit',
+                element: <EditEvent/>,
+            },
+        ],
+    },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router}/>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
